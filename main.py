711f384b2cf9935c372a9f5bfe7b9bb9573ac43f
M1txY <113921255+M1txY@users.noreply.github.com>
2023-11-26 02:31:54 +0100

    finish

--- main.py
+++ main.py
@@ -3,32 +3,58 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import json
 
-def extract_links(base_url, soup):
-    links = []
-    for a_tag in soup.find_all('a', href=True):
+def extract_category_content(category_url):
+    response = requests.get(category_url)
+    category_soup = BeautifulSoup(response.content, 'html.parser')
+
+    main_content = category_soup.find('main')
+    if main_content:
+        content_text = main_content.get_text(separator='\n', strip=True)
+    else:
+        content_text = "Contenu non trouvé"
+    return content_text
+
+def extract_links(base_url, soup, total_links):
+    categories = []
+    for index, a_tag in enumerate(soup.find_all('a', href=True)):
         title = a_tag.get_text().strip()
         link = a_tag['href']
         full_url = urljoin(base_url, link)
-        links.append({'title': title, 'url': full_url})
-    return links
+
+        content_text = extract_category_content(full_url)
+        categories.append({
+            'title': title,
+            'url': full_url,
+            'text': content_text
+        })
+
+        # Mise à jour manuelle de la barre de progression
+        print(f"Traitement des catégories : {index + 1}/{total_links} complété")
+
+    return categories
 
 def extract_main_content(base_url):
-    page = requests.get(base_url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Ici, vous devez utiliser le sélecteur approprié pour la div qui contient les liens
     main_div = soup.find('div', class_='css-175oi2r r-1yzf0co r-1sc18lr')
     if main_div:
-        return extract_links(base_url, main_div)
+        all_a_tags = main_div.find_all('a', href=True)
+        total_links = len(all_a_tags)
+        return extract_links(base_url, main_div, total_links)
     else:
+        print("Erreur : Div spécifiée pour les liens introuvable.")
         return []
 
 def main():
-    base_url = "https://docs.bscscan.com"
+    base_url = "https://docs.bscscan.com"  # Remplacez par l'URL réelle
     categories_data = extract_main_content(base_url)
 
-    with open('data.json', 'w', encoding='utf-8') as f:
+    json_name = '_'.join(base_url.split('//')[1].split('.')[:-1]) + '.json'
+
+    with open(json_name, 'w', encoding='utf-8') as f:
         json.dump(categories_data, f, ensure_ascii=False, indent=4)
+    print(f"Extraction terminée. Données enregistrées dans {json_name}.")
 
 if __name__ == "__main__":
     main()
